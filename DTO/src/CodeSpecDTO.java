f058bb219192d463241f2e2df71b47ac8e00dd41
GuyFirst <guyfirst19@gmail.com>
2025-11-24 13:53:21 +0200

    wrote the DTO objects

---
+++ DTO/src/CodeSpecDTO.java
@@ -0,0 +1,57 @@
+import java.util.List;
+import java.util.Map;
+
+public class CodeSpecDTO {
+    private final int reflectorId;
+    private final List<Integer> currentRotorIds;
+    private final Map<Integer, Integer> rotorPositionOnWindow;
+    private final Map<Integer, Integer> notchPositionFromWindow;
+
+    public CodeSpecDTO(int reflectorId, List<Integer> currentRotorIds,
+                       Map<Integer, Integer> rotorPositionOnWindow,
+                       Map<Integer, Integer> notchPositionFromWindow) {
+        this.reflectorId = reflectorId;
+        this.currentRotorIds = currentRotorIds;
+        this.rotorPositionOnWindow = rotorPositionOnWindow;
+        this.notchPositionFromWindow = notchPositionFromWindow;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CodeSpecDTO that = (CodeSpecDTO) o;
+
+        if (reflectorId != that.reflectorId) return false;
+        if (!currentRotorIds.equals(that.currentRotorIds)) return false;
+        if (!rotorPositionOnWindow.equals(that.rotorPositionOnWindow)) return false;
+        return notchPositionFromWindow.equals(that.notchPositionFromWindow);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = reflectorId;
+        result = 31 * result + currentRotorIds.hashCode();
+        result = 31 * result + rotorPositionOnWindow.hashCode();
+        result = 31 * result + notchPositionFromWindow.hashCode();
+        return result;
+    }
+
+    public int getReflectorId() {
+        return reflectorId;
+    }
+
+    public List<Integer> getCurrentRotorIds() {
+        return currentRotorIds;
+    }
+
+    public Map<Integer, Integer> getRotorPositionOnWindow() {
+        return rotorPositionOnWindow;
+    }
+
+    public Map<Integer, Integer> getNotchPositionFromWindow() {
+        return notchPositionFromWindow;
+    }
+
+}
---
+++ DTO/src/ProcessedOutputDTO.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class ProcessedOutputDTO {
+    private final List<Integer> processedOutputPositions;
+
+    public  ProcessedOutputDTO(List<Integer> processedOutputPositions) {
+        this.processedOutputPositions = processedOutputPositions;
+    }
+
+    public List<Integer> getProcessedOutputPositions() {
+        return processedOutputPositions;
+    }
+
+}
