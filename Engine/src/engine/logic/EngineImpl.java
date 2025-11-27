2d79f1874b0ffb8f7b27a6696cac0bccfc7798e3
GuyFirst <guyfirst19@gmail.com>
2025-11-27 17:06:55 +0200

    engine implementation

---
+++ Engine/src/engine/logic/EngineImpl.java
@@ -0,0 +1,51 @@
+package engine.logic;
+
+import DTO.HistoryDTO;
+import DTO.MachineSpecificationDTO;
+import DTO.MessageDTO;
+
+import java.util.List;
+
+public class EngineImpl implements Engine {
+
+
+    @Override
+    public void loadMachineFromXml(String xmlFilePath) {
+
+    }
+
+    @Override
+    public MachineSpecificationDTO getMachineStatus() {
+        return null;
+    }
+
+    @Override
+    public void setMachineCode(List<Integer> rotorIds, List<Character> positions, String reflectorId) {
+
+    }
+
+    @Override
+    public void setAutomaticCode() {
+
+    }
+
+    @Override
+    public MessageDTO processInput(String inputString) {
+        return null;
+    }
+
+    @Override
+    public void resetMachineToOriginalCode() {
+
+    }
+
+    @Override
+    public HistoryDTO getHistoryAndStatistics() {
+        return null;
+    }
+
+    @Override
+    public boolean isMachineLoaded() {
+        return false;
+    }
+}
