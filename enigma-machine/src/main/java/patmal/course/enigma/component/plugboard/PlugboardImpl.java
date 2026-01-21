37e027e356222d9e08f34d7154731be363d804df
GuyFirst <guyfirst19@gmail.com>
2026-01-21 23:37:28 +0200

    PLUGBOARD IMPLEMENTS SERIELIZBLEEEEEE

--- enigma-machine/src/main/java/patmal/course/enigma/component/plugboard/PlugboardImpl.java
+++ enigma-machine/src/main/java/patmal/course/enigma/component/plugboard/PlugboardImpl.java
@@ -3,9 +3,10 @@ package patmal.course.enigma.component.plugboard;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.Serializable;
 import java.util.Map;
 
-public class PlugboardImpl implements  Plugboard {
+public class PlugboardImpl implements  Plugboard, Serializable {
     private final Map<Character, Character> wiringMap;
     public static final Logger logger = LogManager.getLogger(PlugboardImpl.class);
 
